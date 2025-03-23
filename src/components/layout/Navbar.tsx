37b91ee1851db987d7eb55a7a17e72bdbfd72010
Aditya <adityac5102@gmail.com>
2025-03-23 16:14:09 +0530

    l

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useScrollProgress } from "@/utils/scrollUtils";
 import { cn } from "@/lib/utils";
 import { Zap, Sparkles, X } from "lucide-react";
